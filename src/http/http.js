798081e197419f21165c8abdc1ad53d93e997f9d
StuMisaka <husteiczhu@163.com>
2020-04-29 21:45:48 +0800

    :rocket: add and set axios

---
+++ src/http/http.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = 'http://49.234.136.73:5050';
+
+
+axios.interceptors.request.use(
+  config => {
+  if(localStorage.token){
+    config.headers["Authorization"] = `Bearer ${localStorage.token}`;
+  }
+  return config;
+},error => {
+  return Promise.reject(error);
+})
+
+export default axios
+
+
+
+
