56b76d638040746f17ee5420a1018ad0e22aef03
StuMisaka <husteiczhu@163.com>
2020-05-05 14:22:33 +0800

    :wrench: update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index.js'
+import store from './store/store.js'
+import axios from './http/http.js'
+
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  axios,
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
