d0157cf6f1efcc054454f5acb3d766502bd12cfa
StuMisaka <husteiczhu@163.com>
2020-05-05 16:27:48 +0800

    :bug: delete config about url

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,17 +1,44 @@
 <template>
 <div class='search'>
-  <div>测试方块 搜索</div>
+  <input type="text" name="" id="">
+  <img src="../assets/search.png" alt="">
 </div>
 </template>
 
 <script>
 export default {
+  name:'search'
   
 }
 </script>
 
 <style scoped>
+.search{
+  margin-top: 3vh;
+  margin-bottom: 2vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40vw;
+  height: 5vh;
+  border-radius: 2.5vh;
+  border: 1px solid #EEE9E9;
+  background-color: white;
+}
 
+.search input{
+  flex: 0 0 auto;
+  width: 85%;
+  height: 4vh;
+  font-size: 20px;
+  border: 0;
+  outline: none;
+}
+
+.search img{
+  height: 35px;
+  cursor: pointer;
+}
 </style>
 
 
